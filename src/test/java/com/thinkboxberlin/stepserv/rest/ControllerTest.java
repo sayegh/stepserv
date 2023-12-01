300d54ef75f46465829e8d4516fd109309953f32
sayegh <s@yegh.de>
2023-12-01 12:44:56 +0100

    Import cleanup.

--- src/test/java/com/thinkboxberlin/stepserv/rest/ControllerTest.java
+++ src/test/java/com/thinkboxberlin/stepserv/rest/ControllerTest.java
@@ -1,19 +1,12 @@
 package com.thinkboxberlin.stepserv.rest;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import com.thinkboxberlin.stepserv.Application;
 import com.thinkboxberlin.stepserv.repository.AgentRepository;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK, classes = Application.class)
 @AutoConfigureMockMvc
@@ -25,6 +18,7 @@ public class ControllerTest {
     private AgentRepository agentRepository;
 
     /* Temporarily disabled TODO - authentication based testing
+     * This should be an integration test with an in-memory database.
     @ParameterizedTest
     // @ValueSource(strings = {"/all", "/get-agent-data"}) // six numbers
     @ValueSource(strings = {"/all"}) // six numbers
