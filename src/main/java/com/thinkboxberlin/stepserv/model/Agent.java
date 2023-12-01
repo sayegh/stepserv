b603ddb1667232d4e295c2141447e131de21aaa5
sayegh <s@yegh.de>
2023-12-01 10:13:03 +0100

    Added tags to Agent model.

--- src/main/java/com/thinkboxberlin/stepserv/model/Agent.java
+++ src/main/java/com/thinkboxberlin/stepserv/model/Agent.java
@@ -1,8 +1,12 @@
 package com.thinkboxberlin.stepserv.model;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Index;
 import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -10,7 +14,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "agents")
+@Table(name = "agents",
+    indexes = { @Index(name = "lastseen_index", columnList = "lastSeen", unique = false) })
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
@@ -21,4 +26,6 @@ public class Agent {
     String agentName;
     Date lastSeen;
     String currentLocation;
+    @ElementCollection
+    List<String> tags = new ArrayList<String>();
 }
